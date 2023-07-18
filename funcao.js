c47882c12be4677d2cabb31700076c81468df30e
Lucas Da Silva Garcia <lsgarcia.306@gmail.com>
2023-07-18 12:02:40 -0400

    Arrow Functions

--- funcao.js
+++ funcao.js
@@ -1,3 +1,5 @@
+// Função Simples:
+
 function calculaDoisNumeros (numero1, numero2){
     console.log(numero1 + numero2)
 }
@@ -7,3 +9,18 @@ calculaDoisNumeros(100,200)
 calculaDoisNumeros(50,90)
 calculaDoisNumeros(75,96)
 calculaDoisNumeros(47,29)
+
+
+
+// Arrow Functions:
+
+const calculaTresNumeros = (numero1, numero2, numero3) => {
+    console.log(numero1 + numero2 + numero3)
+}
+
+calculaTresNumeros(10,20,50)
+calculaTresNumeros(50,90,20)
+calculaTresNumeros(100,200,52)
+calculaTresNumeros(75,96,51)
+calculaTresNumeros(47,29,63)
+
