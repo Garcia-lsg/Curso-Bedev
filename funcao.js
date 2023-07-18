130841a34f39156eef1d22b089f27edc396cb8a7
Lucas Da Silva Garcia <lsgarcia.306@gmail.com>
2023-07-18 11:51:38 -0400

    Função simples

---
+++ funcao.js
@@ -0,0 +1,9 @@
+function calculaDoisNumeros (numero1, numero2){
+    console.log(numero1 + numero2)
+}
+
+calculaDoisNumeros(10,20)
+calculaDoisNumeros(100,200)
+calculaDoisNumeros(50,90)
+calculaDoisNumeros(75,96)
+calculaDoisNumeros(47,29)
